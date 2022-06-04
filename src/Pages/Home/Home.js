7bd43499804cb4d97343f04b5b6ec8a690b3b6ff
alamindjp <alaminhossaindjp@gmail.com>
2022-06-05 03:11:05 +0600

    delete the checking div

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -13,7 +13,6 @@ const Home = () => {
         <div>
             <Navbar />
             <HomeBanner />
-            <div>Checking git</div>
             <div className='divider my-10'></div>
             <Tools />
             <div className='divider my-10'></div>
